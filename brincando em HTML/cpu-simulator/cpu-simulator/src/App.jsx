ee828fbc8aee1e021afa2fd6487df896136b6b0b
Enzo Nascimento <dev.enzody@gmail.com>
2024-03-11 09:11:36 -0300

    att

--- brincando em HTML/cpu-simulator/cpu-simulator/src/App.jsx
+++ brincando em HTML/cpu-simulator/cpu-simulator/src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from "react";
 
 import Ram from "./components/Ram/Ram";
 import Cpu from "./components/Cpu/Cpu";
+import Table from "./components/Ram/Table";
 import Settings from "./components/Settings/Settings";
 
 const App = () => {
+
   return (
     <>
       <div className="container">
         <Settings />
         <div className="simulator-container">
           <Cpu />
+          <Table />
           <Ram />
         </div>
       </div>
